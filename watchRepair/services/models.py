67ef0e1722d6574eee4613865e59ab095ca12ddf
wilderSerna78 <176760060+wilderSerna78@users.noreply.github.com>
2025-03-20 16:41:13 -0500

    Inicio del proyecto reparación de relojes

---
+++ watchRepair/services/models.py
@@ -0,0 +1,66 @@
+from django.db import models
+from datetime import date
+
+
+# Create your models here.
+class Watch(models.Model):
+    brand = models.CharField(max_length=100)
+    type_of_machinery = models.CharField(max_length=50)
+    model = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"Marca: {self.brand}, Tipo: {self.type_of_machinery}, Modelo: {self.model}"
+    
+# Modelos especificos de relojes
+class MechanicalWatch(Watch):
+    winding_type = models.CharField(max_length=50) # Tipo reloj Manual o Automático.
+
+class QuartzWatch(Watch):
+    batery_life = models.IntegerField() # Vida de la bateria duracion en años.
+
+class SmartWatch(Watch):
+    os = models.CharField(max_length=50) # Sistema operativo
+    connectivity = models.CharField(max_length=100) #Tipos de conectividad
+
+# Modelos base para personas
+class Person(models.Model):
+    name = models.CharField(max_length=100)
+    identification = models.CharField(max_length=20, unique=True)
+    phone = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"Nombre: {self.name}, ID: {self.identification}, Telefono: {self.phone}"
+    
+# Modelos especificos de personas
+class Customer(Person):
+    email = models.EmailField(unique=True)
+
+class Employee(Person):
+    post = models.CharField(max_length=100) # Cargo
+    salary = models.DecimalField(max_digits=10, decimal_places=2)
+
+class Supplier(Person):
+    company = models.CharField(max_length=100)
+    products = models.TextField() # Lista de productos separados por comas
+
+# Modelo de servicio
+class Service(models.Model):
+    Watch = models.ForeignKey(Watch, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    service_type = models.CharField(max_length=100)
+    cost = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    status = models.CharField(max_length=50, default="Pendiente")
+    observations = models.TextField(blank=True, null=True)
+    parts_used = models.TextField(blank=True, null=True) #Lista de repuestos usados
+    received_date = models.DateField(default=date.today)
+    delivery_date = models.DateField(blank=True, null=True)
+
+    def complete_Service(self):
+        self.status = "Terminado"
+        self.delivery_date = date.today()
+        self.save()
+
+    def __str__(self):
+        return f"Servicio: {self.service_type} | Cliente: {self.customer.name} | Reloj: {self.Watch.brand} - {self.watch.model} | Estado: {self.status} | Costo: ${self.cost}"
+    
+
